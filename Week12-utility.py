203faa0c5013fc0738ebee153d62993dc5bb83e2
Morgan Stoner <morganstoner@mines.edu>
2019-11-17 15:40:28 -0700

    Function Five

--- Week12-utility.py
+++ Week12-utility.py
@@ -30,3 +30,36 @@ def UpdateString(in_string,letter,index):
     my_list[index] = letter
     word = ''.join([str(char) for char in my_list])
     PrintOutput(word)
+
+################################################
+########   Function 4 : FindWordCount  #########
+################################################
+
+def FindWordCount(my_list,in_string):
+    total = 0
+    for i in range(0,len(my_list)):
+        if my_list[i] == in_string:
+            total += 1
+    return total
+
+################################################
+########   Function 5 : FindWordCount  #########
+################################################
+
+def ScoreFinder(players,scores,in_string):
+    string = in_string.lower()
+    check = 'O'
+    for i in range(0,len(players)):
+        check1 = players[i].lower()
+        if check1 == string:
+            check = check1
+            index = i
+    if check == 'O':
+        PrintOutput("player not found")
+        return
+    else:
+        score = scores[index]
+        output = in_string + " got a score of " + str(score)
+        PrintOutput(output)
+
+
